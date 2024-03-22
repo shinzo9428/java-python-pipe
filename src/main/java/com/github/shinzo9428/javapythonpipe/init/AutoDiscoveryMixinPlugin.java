6532aed7603a073d3f022a56167c004172981f9a
GitHub Action <action@github.com>
2024-03-22 02:07:31 +0000

    Initialize template repository proper
    Set package name to com.github.shinzo9428.javapythonpipe
    Set project name to java-python-pipe
    Set modid to javapythonpipe

--- src/main/java/com/github/shinzo9428/javapythonpipe/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/shinzo9428/javapythonpipe/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.shinzo9428.javapythonpipe.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
